c45093071207a92e538e037b2b44f5dc8337fc86
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2024-02-09 12:39:32 +0100

    added menu icons

--- SyncMLViewer/MainWindow.xaml.cs
+++ SyncMLViewer/MainWindow.xaml.cs
@@ -195,7 +195,7 @@ namespace SyncMLViewer
                 BalloonTipTitle = "SyncML Viewer",
                 Text = "SyncML Viewer"
             };
-            Stream iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/;component/sync-icon.ico")).Stream;
+            Stream iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/;component/Icons/sync-icon.ico")).Stream;
             _notifyIcon.Icon = new System.Drawing.Icon(iconStream);
             _notifyIcon.Click += new EventHandler(NotifyIcon_Click);
             _notifyIconBallonShownOnce = false;
